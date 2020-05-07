2fa9ba88ea6179a894853e88eb5e70143ad9cfb8
Denis Smiyanov <zelestik@hotmail.com>
2020-05-07 18:57:01 +0300

    Добавлено взаимодействие с БД

--- src/components/phone_book.vue
+++ src/components/phone_book.vue
@@ -27,11 +27,12 @@
                     favorite: false
                 },
                 notes: [],
-                editing: -1 //состояние при новой записи
+                editing: -1, //состояние при новой записи,
+                cur_id: -1
             }
         },
         methods: {
-            toggleSave: function () {
+            async toggleSave() {
                 if (this.cur_note.phone !== "") {
                     if (this.editing === -1) {
                         this.notes.push({
@@ -39,18 +40,33 @@
                             second_name: this.cur_note.second_name,
                             third_name: this.cur_note.third_name,
                             phone: this.cur_note.phone,
-                            favorite: this.cur_note.favorite});
-                    }
-                    else {
+                            favorite: this.cur_note.favorite
+                        });
+                        try{
+                            await this.$http.post('http://localhost:3000/notes', this.cur_note);
+                            this.updateData();
+                        }catch(e){
+                            console.log(e);
+                        }
+                    } else {
                         this.notes[this.editing] = {
+                            id: this.cur_note.id,
                             first_name: this.cur_note.first_name,
                             second_name: this.cur_note.second_name,
                             third_name: this.cur_note.third_name,
                             phone: this.cur_note.phone,
-                            favorite: this.cur_note.favorite};
+                            favorite: this.cur_note.favorite
+                        };
+                        try{
+                            console.log(this.notes[this.editing]);
+                            await this.$http.put('http://localhost:3000/notes/'+ this.cur_id, this.notes[this.editing]);
+                            this.updateData();
+                        }catch(e){
+                            console.log(e);
+                        }
                         this.editing = -1;
                     }
-                    if(this.sort === "name")
+                    if (this.sort === "name")
                         this.notes.sort((a, b) => a.first_name < b.first_name ? 1 : -1);
                     else if (this.sort === "sec_name")
                         this.notes.sort((a, b) => a.second_name < b.second_name ? 1 : -1);
@@ -60,24 +76,44 @@
                     this.cur_note.third_name = "";
                     this.cur_note.phone = "";
                     this.cur_note.favorite = "";
-                }else{
+                } else {
                     alert("Заметка не может быть пустой");
                 }
             },
-            toggleEdit : function (id) {
-                this.cur_note = this.notes[id];
+            toggleEdit (id) {
+                this.cur_note = {
+                    first_name: this.notes[id].first_name,
+                    second_name: this.notes[id].second_name,
+                    third_name: this.notes[id].third_name,
+                    phone: this.notes[id].phone,
+                    favorite: this.notes[id].favorite
+                };
+                this.cur_id = this.notes[id].id;
                 this.editing = id;
             },
-            toggleDelete : function (id) {
-                this.notes.splice(id, 1);
+            async toggleDelete (id) {
+                await this.$http.delete('http://localhost:3000/notes/' + this.notes[id].id);
+                this.updateData();
+                //this.notes.splice(id, 1);
             },
-            toggleSort : function (value) {
-                if(value === "name")
+            toggleSort (value) {
+                if (value === "name")
                     this.notes.sort((a, b) => a.first_name < b.first_name ? 1 : -1);
                 else if (value === "sec_name")
                     this.notes.sort((a, b) => a.second_name < b.second_name ? 1 : -1);
                 this.notes.sort((a, b) => a.favorite < b.favorite ? 1 : -1);
-            }
+            },
+            updateData() {
+                try {
+                    this.$http.get('http://localhost:3000/notes').then((res) => res.json()).then((res) => (this.notes = res));
+                } catch (e) {
+                    console.error(e);
+                }
+            },
+        },
+        created() {
+            this.toggleSort(this.sort);
+            this.updateData();
         }
     }
 </script>
